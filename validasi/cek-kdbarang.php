bcf130d95acdfdb2a86bd453a56fbb89e4d3a251
totoprayogo1916 <totoprayogo1916@gmail.com>
2022-10-11 16:48:33 +0700

    fix validasi

--- validasi/cek-kdbarang.php
+++ validasi/cek-kdbarang.php
@@ -1,10 +1,13 @@
 <?php
 
-include '../class/class.php';
+require __DIR__ . '/vendor/autoload.php';
+
+$db = new Ataslangit\DB\DataBase();
+
 $kd = $_GET['kdbarang'];
 // Checks if the username is available or not
 $query  = "SELECT kd_barang FROM barang WHERE kd_barang='{$kd}'";
-$result = mysqli_query($this->link, $query);
+$result = mysqli_query($db->sambungkan, $query);
 $jum    = mysqli_num_rows($result);
 if ($jum >= 1) {
     echo "<span style='color:red; padding-left:4px;'><i class='fa fa-warning'></i> Kode Barang Sudah Tersedia</span>";
