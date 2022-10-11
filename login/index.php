1ad1496c7d275e79e0c3736a48714e9900797bb9
totoprayogo1916 <totoprayogo1916@gmail.com>
2022-10-11 16:50:21 +0700

    format

--- login/index.php
+++ login/index.php
@@ -2,7 +2,6 @@
 session_start();
 require __DIR__ . '../../vendor/autoload.php';
 
-// include '../class/class.php';
 $admin = new Ataslangit\DB\Admin();
 
 if (isset($_POST['login'])) {
@@ -10,42 +9,44 @@ if (isset($_POST['login'])) {
 
     if ($cek === true) {
         echo "<script>window.location='../index.php';</script>";
-    }// jika salah atau tidak benar maka login ulang
+    } // jika salah atau tidak benar maka login ulang
     else {
         echo "<script>alert('Login Gagal, Password / Email Salah!');</script>";
     }
 }
 ?>
 <!DOCTYPE html>
-<html >
-  <head>
+<html>
+
+<head>
     <meta charset="UTF-8">
     <title>Inventory Login</title>
     <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <script src="js/prefixfree.min.js"></script>
-  </head>
-  <body>
+</head>
+
+<body>
     <div id="logo">
-      <h1><i>INVENTORY BARANG</i></h1>
+        <h1><i>INVENTORY BARANG</i></h1>
     </div>
-  <section class="stark-login">
-    <form method="POST">
-      <div id="fade-box">
-        <input type="text" name="username" id="username" placeholder="Masukan Email" required>
-        <input type="password" name="password" placeholder="Masukan Password" required>
-        <button name="login">Log In</button>
-      </div>
-    </form>
+    <section class="stark-login">
+        <form method="POST">
+            <div id="fade-box">
+                <input type="text" name="username" id="username" placeholder="Masukan Email" required>
+                <input type="password" name="password" placeholder="Masukan Password" required>
+                <button name="login">Log In</button>
+            </div>
+        </form>
         <div class="hexagons">
         </div>
-  </section>
-  <div id="circle1">
-    <div id="inner-cirlce1">
-      <h2></h2>
+    </section>
+    <div id="circle1">
+        <div id="inner-cirlce1">
+            <h2></h2>
+        </div>
     </div>
-  </div>
     <script src="js/index.js"></script>
-  </body>
-</html>
+</body>
 
+</html>
